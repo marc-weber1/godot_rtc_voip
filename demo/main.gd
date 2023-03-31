eecc5e2a717d031fedd85d25c142f875b55894ac
Marc <marc.weber54@gmail.com>
2023-03-31 03:21:10 -0600

    Attempt to update demo

--- demo/main.gd
+++ demo/main.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+var user = load("res://user.tscn")
+
+var peers = {}
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,7 +12,11 @@ func _ready():
 
 
 func _on_voip_client_user_connected(peer: WebRTCPeerConnection, audio_stream: AudioStreamVOIP):
-	pass
+	var scene_instance = user.instance()
+	add_child(scene_instance)
+	scene_instance.Stream = audio_stream
+	
+	peers[peer] = scene_instance
 
 
 func _on_voip_client_user_disconnected(peer: WebRTCPeerConnection):
